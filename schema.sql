e2069a75b051dfa5bf4d0743c5e4837e1492c597
Arthur <arthurbrazfarias@gmail.com>
2023-06-14 16:15:57 -0300

    Added prototype SQL schema

---
+++ schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users (
+    id INTEGER NOT NULL,
+    username TEXT NOT NULL,
+    hash TEXT NOT NULL,
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE passwords (
+    id INTEGER NOT NULL,
+    user_id INTEGER,
+    domain TEXT NOT NULL,
+    hash TEXT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
