76579b7becc0fdf5b608984e507ba31cde99b63c
Arthur <arthurbrazfarias@gmail.com>
2023-06-16 12:13:25 -0300

    Updated database schema

--- schema.sql
+++ schema.sql
@@ -3,12 +3,13 @@ CREATE TABLE users (
     username TEXT NOT NULL,
     hash TEXT NOT NULL,
     PRIMARY KEY(id)
-)
+);
 
 CREATE TABLE passwords (
     id INTEGER NOT NULL,
     user_id INTEGER,
     domain TEXT NOT NULL,
+    username TEXT NOT NULL,
     hash TEXT NOT NULL,
     PRIMARY KEY(id),
     FOREIGN KEY(user_id) REFERENCES users(id)
